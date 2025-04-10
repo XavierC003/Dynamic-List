bdd0f7b26a2986811b2d9df0e7a792ed58580102
XavierC003 <xaviercollege3@gmail.com>
2025-04-10 11:53:13 -0400

    Added insert, get, delete_element, insert_at_index

--- src/dynamic_list.rs
+++ src/dynamic_list.rs
@@ -43,5 +43,51 @@ impl<T: PartialEq + Copy> DynamicList<T> {
     
         None
     }
+
+    pub fn delete_element(&mut self, data: T) -> bool {
+        let mut current = self.head.as_mut();
+        let mut prev: Option<&mut Box<Node<T>>> = None;
+
+        while let Some(node) = current {
+            if node.data == data {
+                match prev {
+                    Some(prev_node) => {
+                        prev_node.next = node.next.take();
+                    }
+                    None => {
+                        self.head = node.next.take();
+                    }
+                }
+                return true;
+            }
+            prev = Some(current);
+            current = node.next.as_mut();
+        }
+
+        false
+    }
+
+    pub fn insert_at_index(&mut self, index: usize, data: T) -> bool {
+        if index == 0 {
+            let new_node = Box::new(Node { data, next: self.head.take() });
+            self.head = Some(new_node);
+            return true;
+        }
+
+        let mut current = self.head.as_mut();
+        let mut i = 0;
+
+        while let Some(node) = current {
+            if i == index - 1 {
+                let new_node = Box::new(Node { data, next: node.next.take() });
+                node.next = Some(new_node);
+                return true;
+            }
+            i += 1;
+            current = node.next.as_mut();
+        }
+
+        false
+    }
     
 }
